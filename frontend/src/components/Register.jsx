b47159d3a9c49a7bea5b4d35ae4d1b42630b59c2
yangyu <yang@yu.com>
2025-12-24 13:19:53 +0800

    Update the background

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -26,7 +26,7 @@ const Register = () => {
 
   return (
     <div className="login-background">
-      <div className="login-container">
+      <div className="login-container" style={{ background: 'transparent' }}>
         <h1 className="login-title">注册</h1>
         <Form
           name="registerForm"
